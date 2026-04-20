66a6049d28ed2e81ba29b6e2e18b120e7969c266
Alok <optimisticclown098@gmail.com>
2026-04-20 12:31:14 +0530

    updated-cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://your-vercel-url.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json());
 
 app.use('/api/session', sessionRoutes);
